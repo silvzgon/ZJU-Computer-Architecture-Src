26d3063a5ba83f7612d32aca284dcf9f29ad4f75
silvzgon <141477869+silvzgon@users.noreply.github.com>
2024-11-11 01:14:37 +0800

    Update MUX4T1_32.v

--- Lab 3&4/design_sources/common/MUX4T1_32.v
+++ Lab 3&4/design_sources/common/MUX4T1_32.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2020/11/09 15:47:19
-// Design Name: 
-// Module Name: MUX4T1_32
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module MUX4T1_32(
     input [1:0]s,
