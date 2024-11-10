0af94cf773473f0174b01d1c5c093b6cc7d3b9a6
silvzgon <141477869+silvzgon@users.noreply.github.com>
2024-11-10 19:42:22 +0800

    Update HazardDetectionUnit.v

--- Lab 3&4/design_sources/core/HazardDetectionUnit.v
+++ Lab 3&4/design_sources/core/HazardDetectionUnit.v
@@ -7,7 +7,7 @@ module HazardDetectionUnit(
     input[4:0] rd_EXE, rd_MEM, rs1_ID, rs2_ID, rs2_EXE,
     input cmu_stall,
     output PC_EN_IF, reg_FD_EN, reg_FD_stall, reg_FD_flush,
-        reg_DE_EN, reg_DE_flush, reg_EM_EN, reg_EM_flush, reg_MW_EN, reg_MW_flush,
+           reg_DE_EN, reg_DE_flush, reg_EM_EN, reg_EM_flush, reg_MW_EN, reg_MW_flush,
     output forward_ctrl_ls,
     output[1:0] forward_ctrl_A, forward_ctrl_B
 );
@@ -22,11 +22,11 @@ module HazardDetectionUnit(
     reg[1:0] hazard_optype_EXE, hazard_optype_MEM;
     always@(posedge clk) begin
         hazard_optype_MEM <= hazard_optype_EXE & {2{~reg_EM_flush}};
-        hazard_optype_EXE <= hazard_optype_ID & {2{~reg_DE_flush}};
+        hazard_optype_EXE <= hazard_optype_ID  & {2{~reg_DE_flush}};
     end
 
-    parameter hazard_optype_ALU = 2'd1;
-    parameter hazard_optype_LOAD = 2'd2;
+    parameter hazard_optype_ALU   = 2'd1;
+    parameter hazard_optype_LOAD  = 2'd2;
     parameter hazard_optype_STORE = 2'd3;
 
     wire rs1_forward_1     = rs1use_ID && rs1_ID == rd_EXE && rd_EXE && hazard_optype_EXE == hazard_optype_ALU;
@@ -56,7 +56,8 @@ module HazardDetectionUnit(
                             {2{rs2_forward_2}} & 2'd2 |
                             {2{rs2_forward_3}} & 2'd3 ;
 
-    assign forward_ctrl_ls = rs2_EXE == rd_MEM && hazard_optype_EXE == hazard_optype_STORE
-                            && hazard_optype_MEM == hazard_optype_LOAD;
+    assign forward_ctrl_ls = rs2_EXE == rd_MEM                        &&
+                             hazard_optype_EXE == hazard_optype_STORE &&
+                             hazard_optype_MEM == hazard_optype_LOAD  ;
 
 endmodule
